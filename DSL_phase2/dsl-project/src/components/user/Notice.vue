ea74139411b97b3213e204292ec1d3ab20272848
Bawornluk <165368952+Bawornluk@users.noreply.github.com>
2025-02-10 13:25:59 +0700

    ลบคำเตือน

--- DSL_phase2/dsl-project/src/components/user/Notice.vue
+++ DSL_phase2/dsl-project/src/components/user/Notice.vue
@@ -24,7 +24,7 @@ defineProps({
         <div class="text-center">
         <h1 class="text-xl font-extrabold  sm:text-4xl md:text-6xl mb-3">{{title}}</h1>
           <h1 class="text-2xl font-extrabold  sm:text-4xl md:text-6xl mb-20">เวลา <span class="text-indigo-900">{{ time }}</span></h1>
-          <p class="font-bold text-sm  sm:text-lg md:text-2xl mb-2"><span class="text-red-500">คำเตือน</span> หน้าเว็บนี้ <span class="text-red-500">เป็นการจองวันเท่านั้น</span> ส่วนการจองคิวต้องไปสแกน QR CODE หน้าห้อง กยศ.</p>
+          <!-- <p class="font-bold text-sm  sm:text-lg md:text-2xl mb-2"><span class="text-red-500">คำเตือน</span> หน้าเว็บนี้ <span class="text-red-500">เป็นการจองวันเท่านั้น</span> ส่วนการจองคิวต้องไปสแกน QR CODE หน้าห้อง กยศ.</p> -->
           <p class="font-bold text-sm  sm:text-lg md:text-2xl">{{ subtitle }}</p>
         </div>
       </div>
